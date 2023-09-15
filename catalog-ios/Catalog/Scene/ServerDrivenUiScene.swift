494ec741b67edf04cd1d6fb9653b3fb92d004625
Ahmed El-Helw <ahmedre@gmail.com>
2023-09-15 09:16:21 -0400

    Support dynamic URLs for Server Driven UI on iOS

--- catalog-ios/Catalog/Scene/ServerDrivenUiScene.swift
+++ catalog-ios/Catalog/Scene/ServerDrivenUiScene.swift
@@ -6,19 +6,80 @@
 //
 
 import SwiftUI
+import Combine
+
+@MainActor final class ContentViewModel: ObservableObject {
+  @Published var dynamicModel: SDUIRoot
+  private var cancellables = Set<AnyCancellable>()
+  
+  init() {
+    let decoder = JSONDecoder()
+    dynamicModel = try! decoder.decode(SDUIRoot.self, from: jsonData)
+  }
+  
+  func fetchUI(url: String) {
+    URLSession.shared
+      .dataTaskPublisher(for: URL(string: url)!)
+      .tryMap { (data, _) in
+        let decoder = JSONDecoder()
+        let model = try decoder.decode(SDUIRoot.self, from: data)
+        return model
+      }
+      .catch { _ in
+        let decoder = JSONDecoder()
+        let model = try! decoder.decode(SDUIRoot.self, from: errorJsonData)
+        return Just(model)
+      }
+      .receive(on: RunLoop.main)
+      .sink { model in
+        self.dynamicModel = model
+      }
+      .store(in: &cancellables)
+  }
+}
+
+private let jsonData = """
+{
+  "version": 1,
+  "root": {
+    "type": "label",
+    "id": "loading-label",
+    "text": "Salam Droidcon!",
+    "typography": "Body.Medium",
+    "color": "primary"
+  }
+}
+""".data(using: .utf8)!
+
+private let errorJsonData = """
+{
+  "version": 1,
+  "root": {
+    "type": "label",
+    "id": "error-label",
+    "text": "Failed to parse response!",
+    "typography": "Body.Medium",
+    "color": "tertiary"
+  }
+}
+""".data(using: .utf8)!
+
 
 struct ServerDrivenUiScene: View {
+  @State private var url: String = "http://localhost:8000/response.json"
+  @StateObject var viewModel = ContentViewModel()
+
   var body: some View {
-    let payload = fullSampleJson
-    if let data = payload.data(using: .utf8) {
-      let decoder = JSONDecoder()
-      let content = try? decoder.decode(SDUIRoot.self, from: data)
-      if content != nil {
-        content
-      } else {
-        Text("Error")
+    VStack {
+      TextField("url", text: $url)
+        .textFieldStyle(.roundedBorder)
+      Button("Refresh") {
+        viewModel.fetchUI(url: url)
       }
     }
+    
+    viewModel.dynamicModel
+      .frame(maxHeight: .infinity)
   }
 }
 
